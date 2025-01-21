3cf7211d6b2f346879b2b7a15be96a1f2a2dc503
rramprabhakar@gmail.com <rramprabhakar@gmail.com>
2025-01-21 15:14:39 -0700

    changing pic

--- dist/rams-portfolio/server/main.server.mjs
+++ dist/rams-portfolio/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VYR4F7DW.mjs";import"./chunk-K7AGIIIJ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HLCRJPBT.mjs";import"./chunk-K7AGIIIJ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
